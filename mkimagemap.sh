aa61209f350428b30d3665383319e8ede3276151
jleen <jleen@saturnvalley.org>
2005-03-31 09:50:08 +0000

    I woke up in the middle of the night and realized I had a quotation bug in my shell script.  Does this ever happen to anyone else?
    git-svn-id: https://saturnvalley.org/svn/lexgraph/trunk@467 e8952cfc-6410-0410-bd16-81fef15e32ef

--- mkimagemap.sh
+++ mkimagemap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python lexiconGrapher.py > $1.dot
-dot -Tpng $1.dot > $1.png
-echo '<img border="0" src="lexgraph.png" usemap="#lexicon">' > $1.html
-dot -Tcmapx $1.dot >> $1.html
+python lexiconGrapher.py > "$1.dot"
+dot -Tpng "$1.dot" > "$1.png"
+echo "<img border=\"0\" src=\"$1.png\" usemap=\"#lexicon\">" > "$1.html"
+dot -Tcmapx "$1.dot" >> "$1.html"
