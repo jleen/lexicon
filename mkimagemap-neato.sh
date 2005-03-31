bf880a4a9b8726ce855dde5725e88d4d35c02cb0
draythan <draythan@gmail.com>
2005-03-31 21:32:13 +0000

    I think this may be as good as it gets. Very crowded in the middle, but far fewer dramatic outliers.
    git-svn-id: https://saturnvalley.org/svn/lexgraph/trunk@469 e8952cfc-6410-0410-bd16-81fef15e32ef

--- mkimagemap-neato.sh
+++ mkimagemap-neato.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python lexiconGrapher.py > "$1.dot"
-neato -Tpng -Gepsilon=.00001 -Gsplines=true -Gmaxiter=30000 -Gpack=true -Gsep=.1 -Goverlap=false "$1.dot" > "$1.png"
+neato -Tpng -Gsplines=true -Gstart=self -Goverlap=false "$1.dot" > "$1.png"
 echo "<img border=\"0\" src=\"$1.png\" usemap=\"#lexicon\">" > "$1.html"
-neato -Tcmapx -Gepsilon=.00001 -Gsplines=true -Gmaxiter=30000 -Gpack=true -Gsep=.1 -Goverlap=false "$1.dot" >> "$1.html"
+neato -Tcmapx -Gsplines=true -Gstart=self -Goverlap=false "$1.dot" >> "$1.html"
